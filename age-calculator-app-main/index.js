7af8b061530c6dedbd4913bef90a849668ec5851
Dantochi <57640426+Dantochi@users.noreply.github.com>
2024-06-03 18:22:39 +0100

    Implemented the logic for calculating age in years, month, day and implemented part for the validation error

--- age-calculator-app-main/index.js
+++ age-calculator-app-main/index.js
@@ -8,10 +8,13 @@ refresh the values of the form input
 
 */
 
+// How to get the date?
+// 
+
 
 let date = new Date();
-let days = 365;
-let month = 12;
+let days = date.getDate();
+let current_month = date.getMonth(); // Here the month starts from 0 instead of one.
 let current_year = date.getFullYear();
 let years_span = document.getElementById("year-value");
 let month_span = document.getElementById("month-value");
@@ -19,15 +22,58 @@ let day_span = document.getElementById("day-value");
 let button = document.querySelector("button");
 
 button.addEventListener('click', function(event){
-    event.preventDefault();
+    event.preventDefault(); // This is used to prevent the form default of submitting and leaving the form empty
     updateDate();
 });
 
 function updateDate(){
-    let dayFormValue = Number(document.getElementById('day').value);
-    let monthFormValue = Number(document.getElementById('month').value);
-    let yearFormValue = Number(document.getElementById('year').value);
-    years_span.innerHTML = current_year - yearFormValue
-    month_span.innerHTML = month - monthFormValue
-    day_span.innerHTML = days - dayFormValue
+    let birthday = Number(document.getElementById('day').value);
+    let birth_month = Number(document.getElementById('month').value);
+    let birth_year = Number(document.getElementById('year').value);
+    day_span.innerHTML = days
+    // Checking for empty birthday input
+    if (birthday === 0){
+        let day_error = document.getElementById('day_error')
+        day_error.classList.add('validation_error_message')
+    }
+    else{
+        day_error.classList.remove('validation_error_message')
+    }
+    // Checking for empty birth_month input
+    if (birth_month === 0){
+        let month_error = document.getElementById('month_error')
+        month_error.classList.add('validation_error_message')
+    }
+    else{
+        month_error.classList.remove('validation_error_message')
+    }
+    // Checking for empty birth_year input
+    if (birth_year === 0){
+        let year_error = document.getElementById('year_error')
+        year_error.classList.add('validation_error_message')
+    }
+    else{
+        year_error.classList.remove('validation_error_message')
+    }
+    // Calculating age in years and months if form is not empty
+    if(birth_month <= current_month){
+        years_span.innerHTML = current_year - birth_year;
+        month_span.innerHTML = Math.abs(birth_month - current_month);
+    } else if (birth_month > current_month + 1) {
+        years_span.innerHTML = current_year - (birth_year + 1)
+        month_span.innerHTML = Math.abs(12 - (birth_month - current_month));
+    }
+    
+    
 }
+
+
+// Things to do
+/*
+Find out how to attach a class to an element
+If any of the forms are null attach the class validation error message
+If all of the forms are empty attach the class validation error message to all the inputs
+
+
+
+*/
